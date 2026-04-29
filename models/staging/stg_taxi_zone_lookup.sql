23952a946c724c9e83f7a001c01b0420ed4c5041
Rainier Moreno <rainier.moreno.c@uni.pe>
2026-04-29 23:11:40 +0000

    modelos y primera macro

---
+++ models/staging/stg_taxi_zone_lookup.sql
@@ -0,0 +1,6 @@
+select
+    cast(locationid as integer) as location_id,
+    cast(borough as string) as borough,
+    cast(zone as string) as zone,
+    cast(service_zone as string) as service_zone
+from {{ source('raw_data', 'taxi_zone_lookup') }}
