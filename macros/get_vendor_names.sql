d2e544a3623f5550c9c27412978a8aa120fbca90
Rainier Moreno <rainier.moreno.c@uni.pe>
2026-04-30 01:07:22 +0000

    documentacion y reports

--- macros/get_vendor_names.sql
+++ macros/get_vendor_names.sql
@@ -1,3 +1,13 @@
+{#
+    Macro to generate vendor_name column using Jinja dictionary.
+
+    This approach works seamlessly across BigQuery, DuckDB, Snowflake, etc.
+    by generating a CASE statement at compile time.
+
+    Usage: {{ get_vendor_data('vendor_id') }}
+    Returns: SQL CASE expression that maps vendor_id to vendor_name
+#}
+
 {% macro get_vendor_names(vendor_id) -%}
 case
     when {{ vendor_id }} = 1 then 'Creative Mobile Technologies'
